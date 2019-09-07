ec449d29a712c8786e4567ad83484b6be50a9f1d
Pitis <pitis.radu@gmail.com>
2019-09-07 17:20:21 +0300

    first screen done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Popup from './Popup'
 import './App.css';
 
 const App = () => {
+  const [feedbackType, setFeedbackType] = useState('')
+
+  const showPopup = (data) => {
+    setFeedbackType(data)
+  }
+
   return (
     <div className="App">
-  
+      <header>
+        <img src={require("./assets/imgs/logo.png")} alt=""/>
+        <p>Feedback-ul dvs. este important pentru noi</p>
+      </header>
+      <div className="layout-grid">
+        <button className="btn-primary" onClick={() => showPopup('hartie')}><i className="fas fa-toilet-paper"></i>Hartie igienica</button>
+        <button className="btn-primary" onClick={() => showPopup('sapun')}><i className="fas fa-hand-holding"></i>Sapun</button>
+        <button className="btn-primary" onClick={() => showPopup('cos-gunoi')}><i className="fas fa-trash"></i>Cos de Gunoi</button>
+        <button className="btn-primary" onClick={() => showPopup('wc-murdar')}><i className="fas fa-toilet"></i>WC Murdar</button>
+      </div>
+      {feedbackType && <Popup feedback={feedbackType} />}
     </div>
   )
 }
---
+++ src/Popup.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Popup = ({feedback}) => {
+    return (
+        <div className="popup">
+            <div className="info-popup">
+                <h1>feedback</h1>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Popup
